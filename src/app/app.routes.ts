e2debeab1b0269419c716611fda24f0e6beb41d3
Carlos Nazario <carlosnazario2010cn@gmail.com>
2024-10-16 19:48:10 -0300

    criando a pagina de home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
     {
         path: 'login',
         component: LoginComponent,
