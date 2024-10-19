6f1e366c6af94bc34c95199afe3e1d3097c66a6e
Carlos Nazario <carlosnazario2010cn@gmail.com>
2024-10-19 11:05:56 -0300

    implementando a funcionalidade de criar carteira com id do cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
@@ -24,5 +25,6 @@ export const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent,
+        canActivate: [AuthGuard] // Adiciona o AuthGuard à rota
     },
 ];
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
-import { FooterComponent } from "../../components/footer/footer.component";
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
     selector: 'app-dashboard',
@@ -10,6 +11,36 @@ import { FooterComponent } from "../../components/footer/footer.component";
     styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-    nomeUsuario = sessionStorage.getItem('user-name');
-    idUsuario = sessionStorage.getItem('user-id');
+
+    clienteNome = sessionStorage.getItem('user-name');
+    clienteId = sessionStorage.getItem('user-id');
+    token = "Bearer " + sessionStorage.getItem("auth-token");
+
+    constructor(private http: HttpClient) {}
+
+    header: HttpHeaders = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': this.token
+    });
+
+    criarCarteira() {
+        const data = {
+            clienteId: this.clienteId,
+        };
+
+        this.http
+            .post('http://localhost:8080/carteiras', data, {
+                headers: this.header
+            })
+            .subscribe(
+                (response) => {
+                    console.log('Carteira criada com sucesso:', response);
+                    // Aqui você pode adicionar alguma lógica para mostrar uma mensagem de sucesso ao usuário
+                },
+                (error) => {
+                    console.error('Erro ao criar carteira:', error);
+                    // Aqui você pode adicionar alguma lógica para mostrar uma mensagem de erro ao usuário
+                }
+            );
+    }
 }
